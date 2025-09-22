fa48fcc845db79a67a11cc5f8400224baa46f41b
Susan S. <susan.sotosarmiento@mygeorgian.ca>
2025-09-22 11:18:05 -0400

    finishedLAB2

--- comp2112-2025-master/src/app/contact/page.tsx
+++ comp2112-2025-master/src/app/contact/page.tsx
@@ -3,25 +3,37 @@
 import { useState } from "react";
 
 export default function Contact() {
-    const [ name, setName] = useState<string>('');
+  //2. const to set state
+  const [ name, setName ] = useState<string>("");
 
-    // watch name input and update state var as input value changes
-    const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setName(event.target.value);
-    }
+  //5. LAB 2: add state variable for message and confirmation
+  const [ message, setMessage ] = useState<string>("");
+  const [ confirmation, setConfirmation ] = useState<string>("");
+  
+  //3. watch name input and update state variable as input value changes
+  const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setName(event.target.value);
+  }
 
-    // show confirmation when button clicked
-    const handleSubmit = () => {
+  //6. LAB 2: add handler for message input
+  const handleMessageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setMessage(event.target.value);
+  }
 
-    }
+  //4. show confirmation when button clicked
+  const handleSubmit = () => {
+    setConfirmation(`Your name: ${name} - Your message: ${message}`);
+  }
 
-    return (
-        <main>
-            <h1>Contact Us</h1>
-            <p>Contact us at 705.000.0000 or send us a message below</p>
-            <input type="text" placeholder="Your Name" value={name} onChange={handleNameChange} />
-            <p>Your name is: {name}</p>
-            <button onClick={handleSubmit}>Submit</button>
-        </main>
-    );
-}
+//1. include return
+  return(  
+    <main>
+      <h1>Contact Us</h1>
+      <p>Contact Us at 705.000.000 </p>
+      <input type="text" placeholder="Your Name" value={name} />
+      <p>Your name is: {name}</p>
+      
+      <button> Submit </button>
+    </main>
+  );
+}  
